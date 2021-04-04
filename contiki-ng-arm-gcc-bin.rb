3402d24b2f5c40308aa8d895552bfeb4fac74faa
Yagoor <yago.rosario@hotmail.com.br>
2021-04-04 11:58:55 +0200

    Updated formula

--- contiki-ng-arm-gcc-bin.rb
+++ contiki-ng-arm-gcc-bin.rb
@@ -2,10 +2,10 @@ require 'formula'
 
 class ContikiNgArmGccBin < Formula
 
-    homepage 'https://launchpad.net/gcc-arm-embedded/'
-    url 'https://launchpad.net/gcc-arm-embedded/5.0/5-2015-q4-major/+download/gcc-arm-none-eabi-5_2-2015q4-20151219-mac.tar.bz2'
-    sha256 '41056ffeba4bcb5bbea13185461a1269613ac13321fbda3e7dc59ee664ee3f06'
-    version '5-2015-q4-major'
+    homepage 'https://developer.arm.com/tools-and-software/open-source-software/developer-tools/gnu-toolchain/gnu-rm/downloads'
+    url 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/9-2019q4/gcc-arm-none-eabi-9-2019-q4-major-mac.tar.bz2'
+    sha256 '1249f860d4155d9c3ba8f30c19e7a88c5047923cea17e0d08e633f12408f01f0'
+    version '9-2019-q4-major'
 
     def install
         bin.install Dir["bin/*"]
